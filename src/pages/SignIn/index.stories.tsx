5ee1a298e66903b3f552edd2398530ac0c55b852
HenrickyL <Henrickyl1@gmail.com>
2022-10-17 00:12:07 -0300

    refactory signin page and doc

---
+++ src/pages/SignIn/index.stories.tsx
@@ -0,0 +1,11 @@
+import {SignIn} from './index'
+import {Meta,StoryObj} from '@storybook/react'
+import React from 'react'
+export default {
+    title: 'Pages/SignIn',
+    component: SignIn,
+  
+} as Meta
+
+
+export const Default:StoryObj={}
